971af14fb8c8fe81908928b44545d848ca682bc4
christopherfanning <christopherleefanning@gmail.com>
2020-12-29 14:57:49 -0600

    Reformat JSON to JS

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 // create an array of the houses
-const houses = ["Engine 1", "Engine 2", "Engine 3", "red"];
+// const houses = ["Engine 1", "Engine 2", "Engine 3", "red"];
 
 // Print out the houses in some formatted way
-printHouses = () => {
-  var newContainer = document.createElement("div");
-  newContainer.id = "houses";
-  document.body.appendChild(newContainer);
+// printHouses = () => {
+//   var newContainer = document.createElement("div");
+//   newContainer.id = "houses";
+//   document.body.appendChild(newContainer);
 
-  for (var h in houses) {
-    var newElement = document.createElement("div");
-    newElement.className = "house";
-    newElement.innerHTML = houses[h];
-    document.getElementById("houses").appendChild(newElement);
-  }
-};
+//   for (var h in houses) {
+//     var newElement = document.createElement("div");
+//     newElement.className = "house";
+//     newElement.innerHTML = houses[h];
+//     document.getElementById("houses").appendChild(newElement);
+//   }
+// };
 
-printHouses();
+function load() {
+  var derp = JSON.parse(houses);
+  console.log(derp[1].name);
+}
+load();
+
+// printHouses();
 
 //  Make some kind of search function with an input box
