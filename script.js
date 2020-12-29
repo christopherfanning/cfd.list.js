80e83dfd66218520bbccbbf0874f22f3426873eb
christopherfanning <christopherleefanning@gmail.com>
2020-12-29 17:15:48 -0600

    Add clickable maps links

--- script.js
+++ script.js
@@ -26,20 +26,29 @@ buildList = () => {
 
   //   }
 
+  //   https://www.google.com/maps/place/419+S+Wells+St,+Chicago,+IL+60607/
+
   for (let i = 0; i < houses.length; i++) {
     // Creates a Div for the house
     var houseContainer = document.createElement("div");
+    const regex = / /gi;
+    mapsLink = houses[i].address.replace(regex, "+");
     houseContainer.className = "house";
     houseContainer.innerHTML =
       "<h3>" +
       houses[i].name +
       "</h3>" +
       "<p>" +
+      "<a href='https://www.google.com/maps/place/" +
+      mapsLink +
+      ",+Chicago,+IL/'>" +
       houses[i].address +
+      "</a>" +
       "</p>" +
       "<p>" +
       houses[i].rigs +
       "</p>";
+
     document.getElementById("houses").appendChild(houseContainer);
   }
 };
