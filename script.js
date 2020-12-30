aeb2b9901df488958479a239496ab8a665e72e7d
christopherfanning <christopherleefanning@gmail.com>
2020-12-29 19:24:10 -0600

    Tidy up script, remove old comments

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-// create an array of the houses
-// const houses = ["Engine 1", "Engine 2", "Engine 3", "red"];
-
-// Print out the houses in some formatted way
-// printHouses = () => {
 var newContainer = document.createElement("div");
 newContainer.id = "houses";
 document.body.appendChild(newContainer);
 
-// for (var h in houses) {
-//   var newElement = document.createElement("div");
-//   newElement.className = "house";
-//   newElement.innerHTML = houses[h];
-//   document.getElementById("houses").appendChild(newElement);
-// }
-// };
-
-function load() {
-  //   console.log(houses[0].name);
-}
-// load();
-
 buildList = () => {
-  //   for (var h in houses) {
-  //     console.log(houses[1].name);
-
-  //   }
-
   //   https://www.google.com/maps/place/419+S+Wells+St,+Chicago,+IL+60607/
 
   for (let i = 0; i < houses.length; i++) {
@@ -57,7 +34,5 @@ buildList = () => {
 };
 
 buildList();
-
-// printHouses();
 
 //  Make some kind of search function with an input box
